0521d937babce4ef33eb79f46421f36660346d79
Yoseph Bernandus <yosephbernandus@gmail.com>
2024-02-21 07:53:44 +0700

    Extends htmx and login (#1)

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.AutoField(db_column='post_id', primary_key=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = 'post'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Comment(models.Model):
+    id = models.AutoField(db_column='comment_id', primary_key=True)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    description = models.TextField()
+
+    db_table = 'comment'
+
+    def __str__(self) -> int:
+        return self.id
